e03b9e568b6f442ddf9a4e3e1cf67ac6eef26cf7
Mohiminul Islam Ahad <122167069+ahad30@users.noreply.github.com>
2024-02-27 18:47:23 +0600

    Ahad

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,10 +1,14 @@
-import banner from '../../assets/banner.png'
 
+import banner from '../../assets/banner.png';
+import Image1 from '../../assets/Delivery-Partner/Foodpanda.png';
+import Image2 from '../../assets/Delivery-Partner/Foodi.png';
+import Image3 from '../../assets/Delivery-Partner/Pathao.png';
+import Menu from '../Menu/Menu';
 
 const HomePage = () => {
   return(
-
-  // Banner section start
+<>
+  {/* // Banner section start */}
 <section
   className="relative  bg-cover bg-center bg-no-repeat"
   style={{ backgroundImage: `url(${banner})` }}
@@ -14,7 +18,7 @@ const HomePage = () => {
   ></div>
 
   <div
-    className="relative mx-auto  py-32  lg:flex lg:justify-center lg:h-screen lg:items-center lg:px-8"
+    className="relative mx-auto  py-32  lg:flex lg:justify-center lg:h-screen lg:items-center lg:px-8 max-w-7xl"
   >
     <div className="text-center">
       <h1 className="text-4xl font-extrabold sm:text-6xl text-white ">
@@ -23,7 +27,7 @@ const HomePage = () => {
 
       <div className="absolute mt-10 left-[83px] lg:mt-48 sm:left-14 flex flex-wrap gap-4">
         <a
-          href="#"
+          href="#Menu"
           className="block w-full  bg-primary px-8 py-3 font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto rounded-full text-sm lg:text-lg"
         >
           আমাদের স্পেশাল মেনূ দেখুন
@@ -32,8 +36,52 @@ const HomePage = () => {
     </div>
   </div>
 </section>
-  // Banner section end
+  {/* // Banner section end */}
 
+
+  {/* Menu Section Start */}
+  <div id='Menu' className='mt-8 mb-16'>
+    <Menu/>
+  </div>
+    {/* Menu Section end */}
+
+   
+   {/* Branch Section Start */}
+              <div></div>
+   {/* Branch Section  End */}
+
+    
+    {/* Delivery Partner Start */}
+    <section className="w-[90%] lg:max-w-7xl mx-auto">
+    <div className='grid grid-cols-1 lg:grid-cols-2'>
+          <div className='flex items-center justify-center'>
+             <h2 className='font-bold text-2xl lg:text-3xl text-[#000]'>আমাদের ডেলিভারি পার্টনার</h2>
+          </div>
+
+          <div className='bg-white rounded-2xl shadow-lg'>
+            <div className='flex justify-center border-2 border-t-0 border-r-0 border-l-0 border-b-gray-200'>
+                <img src={Image1} alt="" className='h-[142px]'/>
+            </div>
+            <div className='flex  items-center  justify-center'>
+            <div className='border-2 border-t-0 border-r-gray-200 border-l-0 border-b-0'>
+              <img src={Image2} alt="" />
+            </div>
+            <div>
+              <img src={Image3} alt="" />
+            </div>
+
+            </div>
+
+          </div>  
+    </div>  
+      
+    </section>
+
+
+    {/* Delivery Partner End */}
+
+
+  </>
   );
 };
 
--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -4,7 +4,7 @@ import Items from "./Items";
 const Menu = () => {
   return (
     <section className="w-[90%] lg:max-w-7xl mx-auto">
-      <div className=" bg-[#FF01010D] px-6 py-16 max-w-6xl mx-auto rounded-xl">
+      <div className=" bg-[#FF01010D] px-6 py-16" style={{ borderTopRightRadius: '250px', borderBottomLeftRadius: '100px' }}>
         <h2 className="text-center text-xl lg:text-3xl font-bold">আমাদের স্পেশাল মেনু</h2>
         <hr className="w-[15%] mx-auto mt-3 bg-black p-[1px]" />
         <Items/>     
